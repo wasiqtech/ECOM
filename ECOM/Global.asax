ead767c89feaf08988365ece16d13844f1473957
wasiqtech <wasiq.bahria84@gmail.com>
2022-01-31 17:26:17 +0500

    Add project files.

---
+++ ECOM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECOM.MvcApplication" Language="C#" %>
---
+++ ECOM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECOM.Startup))]
+namespace ECOM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
